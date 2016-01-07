061fc160ee61ea5f3391e1b6f13e8f0406d65691
florinn <florinn>
2016-01-07 18:16:03 +0200

    fix a TS compilation error
    Fix the following:
    [gulp-typescript] 2345 Argument of type '{}' is not assignable to
    parameter of type 'string'.
    file:
    /Users/zhouhao/Projects/angular/angular-typescript-gulp-seed/app/todo/todo.controller.ts:
    [38] ];
    [00:05:10] TypeScript: 1 semantic error

--- app/todo/todo.controller.ts
+++ app/todo/todo.controller.ts
@@ -35,7 +35,7 @@ module todos {
             // watching for events/changes in scope, which are caused by view/user input
             // if you subscribe to scope or event with lifetime longer than this controller, make sure you unsubscribe.
             $scope.$watch('todos', () => this.onTodos(), true);
-            $scope.$watch('location.path()', path => this.onPath(path))
+            $scope.$watch<string>('location.path()', path => this.onPath(path));
             if ($location.path() === '') $location.path('/');
             $scope.location = $location;
         }
